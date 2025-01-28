f25d6c6079c2330d6838bcbdfd4ca41e9768862d
whoishasan <uniquest.hasan@gmail.com>
2025-01-28 21:52:22 +0600

    mydonation model

--- models/mydonation.model.js
+++ models/mydonation.model.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 
-const myDonationSchema = new mongoose.Schema();
+const myDonationSchema = new mongoose.Schema({
+  ownerUid: {
+    type: String,
+    required: true,
+  },
+  ammount: {
+    type: Number,
+    required: true,
+  },
+  campaign: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "campaigns",
+    required: true,
+  },
+});
 
 const myDonation = mongoose.model("Donations", myDonationSchema);
 
