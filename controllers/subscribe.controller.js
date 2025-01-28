daadcae3c7d9633942d3d3d9778d5fcdaeb31e0b
whoishasan <uniquest.hasan@gmail.com>
2025-01-28 21:47:35 +0600

    subscribe commit

--- controllers/subscribe.controller.js
+++ controllers/subscribe.controller.js
@@ -1,4 +1,22 @@
 import SubscribeUser from "../models/subscribe.model.js";
-const subscribeController = async (req, res) => {};
+const subscribeController = async (req, res) => {
+  try {
+    const email = new SubscribeUser({
+      email: req.body.email,
+    });
+
+    const user = await email.save();
+    res.status(200).send(user);
+  } catch (err) {
+    if (err.code === 11000) {
+      res.status(400).send({
+        code: 11000,
+        msg: "This Email Alredy Exist",
+      });
+    } else {
+      res.status(400).send(err);
+    }
+  }
+};
 
 export { subscribeController };
