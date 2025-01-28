aeaa2a9e00aff3ef69d6bdaf0a287917f25a7c16
whoishasan <uniquest.hasan@gmail.com>
2025-01-28 21:50:09 +0600

    campaign model

--- models/campaign.model.js
+++ models/campaign.model.js
@@ -1,6 +1,53 @@
 import mongoose from "mongoose";
 
-const campaignSchema = new mongoose.Schema();
+const campaignSchema = new mongoose.Schema(
+  {
+    thumb: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    minAmmount: {
+      type: Number,
+      default: 1,
+      required: true,
+    },
+    deadline: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+    },
+    userAvatar: {
+      type: String,
+      required: true,
+    },
+    ownerUid: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Campaign = mongoose.model("campaigns", campaignSchema);
 
