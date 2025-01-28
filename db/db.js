8f4bf4768df2f80b9b17abb3c159444b349aa9e7
whoishasan <uniquest.hasan@gmail.com>
2025-01-28 21:48:23 +0600

    db js commit

--- db/db.js
+++ db/db.js
@@ -1,4 +1,16 @@
 import mongoose from "mongoose";
 
-const mongoDBConnection = async () => {};
+const mongoDBConnection = async () => {
+  try {
+    const conn = await mongoose.connect(
+      `${process.env.MONGO_DB_URL}/crowd_stream`
+    );
+    console.log(
+      `Mongo DB Connection Succesfull To ${conn.connection.db.databaseName}`
+    );
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 export default mongoDBConnection;
